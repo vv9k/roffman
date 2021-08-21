e960fe9ff8c9a97dd0ccefbb62f1f1e5d15f620e
Wojciech Kępka <wojciech@wkepka.dev>
2021-08-21 06:48:34 +0200

    Update the examples

--- README.md
+++ README.md
@@ -15,66 +15,79 @@ roffman = "0.1"
 
 ## Example
 ```rust
-use roffman::{IntoRoffNode, Roff, Roffable, RoffNode};
+use roffman::{Roff, RoffNode, RoffNode, Roffable, SectionNumber, SynopsisOpt};
 
 fn main() {
-    let roff = Roff::new("roffman-manual", 7)
-        .date("August 2021")
-        .section(
-            "BASIC USAGE",
-            vec![
-                RoffNode::paragraph(vec![
-                    "This is how you create a basic paragraph using roffman.",
-                ]),
-                RoffNode::indented_paragraph(
-                    vec![
-                        "This line should be slightly indented to the ".roff(),
-                        "right.".roff().bold(),
-                    ],
-                    Some(4),
-                ),
-                RoffNode::paragraph(vec![
-                    "And some example ".roff(),
-                    "code".roff().italic(),
-                    ":".roff(),
-                ]),
-                RoffNode::text(":"),
-                RoffNode::example(vec![
-                    r#"
+    let roff = Roff::new("roffman", SectionNumber::Miscellaneous)
+    .date("August 2021")
+    .section(
+       "BASIC USAGE",
+       [
+           RoffNode::paragraph([
+               "This is how you create a basic paragraph using roffman.",
+           ]),
+           RoffNode::indented_paragraph(
+               [
+                   "This line should be slightly indented to the ".roff(),
+                   "right.".roff().bold(),
+               ],
+               Some(4),
+           ),
+           RoffNode::synopsis(
+                    "roffman-command",
+                    [
+                    "This is the description of this command. It will be displayed right next to".roff(), " it".roff().italic()
+]                     ,
+                    [
+                    SynopsisOpt::new("--opt").description(["some simple opt"]),
+                    SynopsisOpt::new("--opt-with-arg").argument("ARG").description(["opt with an argument"]),
+                    SynopsisOpt::new("--bold")
+           ]),
+           RoffNode::paragraph(["Example:".roff().bold()]),
+           RoffNode::example([
+               r#"
 impl Roffable for u8 {
     fn roff(&self) -> RoffText {
         self.to_string().roff()
     }
 }"#,
-                ]),
-            ],
-        );
+            ]),
+           RoffNode::url("GitHub", "https://github.com/vv9k/roffman"),
+           RoffNode::text("\nvv9k"),
+           RoffNode::trademark_sign(),
+            
+        ],
+    );
 
     let rendered = roff.to_string().unwrap();
     println!("{}", rendered);
+
 }
+
 ```
 
 Output:
-
 ```roff
-.TH "roffman" "7" "August 2021"
-.
-
+.TH roffman 7 "August 2021"
 .SH "BASIC USAGE"
-
 .P
 This is how you create a basic paragraph using roffman\.
-.
-
 .IP "" 4
 This line should be slightly indented to the \fBright\.\fR
-.
+.SY roffman\-command
+This is the description of this command\. It will be displayed right next to \fIit\fR
 
+.OP \-\-opt
+some simple opt
+
+.OP \-\-opt\-with\-arg ARG
+opt with an argument
+
+.OP \-\-bold
+
+.YS
 .P
-And some example \fIcode\fR:
-.
-
+\fBExample:\fR
 .EX
 
 impl Roffable for u8 {
@@ -83,26 +96,42 @@ impl Roffable for u8 {
     }
 }
 .EE
+.UR https://github\.com/vv9k/roffman
+GitHub
+.UE
+
+vv9k\*(Tm
 ```
 
 which will look something like this:
 ```
-roffman(7)            Miscellaneous Information Manual           roffman(7)
+roffman(7)                                         Miscellaneous Information Manual                                  roffman(7)
 
 BASIC USAGE
        This is how you create a basic paragraph using roffman.
 
            This line should be slightly indented to the right.
 
-       And some example code:
+       roffman-command This is the description of this command. It will be displayed right next to it
+
+                       [--opt] some simple opt
+
+                       [--opt-with-arg ARG] opt with an argument
+
+                       [--bold]
+
+       Example:
 
        impl Roffable for u8 {
            fn roff(&self) -> RoffText {
                self.to_string().roff()
            }
        }
+       GitHub ⟨https://github.com/vv9k/roffman⟩
 
-                                                                 roffman(7)
+       vv9k™
+
+                                                              August 2021                                             roffman(7)
 ```
 
 ## License
--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,37 @@
 //!
 //! ## Example usage
 //! ```
-//! use roffman::{IntoRoffNode, Roff, Roffable, RoffNode, SectionNumber};
+//! use roffman::{Roff, RoffNode, Roffable, SectionNumber, SynopsisOpt};
 //!
-//! let roff = Roff::new("roffman", SectionNumber::Miscellaneous)
+//! fn main() {
+//!     let roff = Roff::new("roffman", SectionNumber::Miscellaneous)
 //!     .date("August 2021")
 //!     .section(
 //!        "BASIC USAGE",
-//!        vec![
-//!            RoffNode::paragraph(vec![
+//!        [
+//!            RoffNode::paragraph([
 //!                "This is how you create a basic paragraph using roffman.",
 //!            ]),
 //!            RoffNode::indented_paragraph(
-//!                vec![
+//!                [
 //!                    "This line should be slightly indented to the ".roff(),
 //!                    "right.".roff().bold(),
 //!                ],
 //!                Some(4),
 //!            ),
-//!            RoffNode::paragraph(vec![
-//!                "And some example ".roff(),
-//!                "code".roff().italic(),
-//!                ":".roff(),
+//!            RoffNode::synopsis(
+//!                     "roffman-command",
+//!                     [
+//!                     "This is the description of this command. It will be displayed right next to".roff(),
+//! " it".roff().italic()
+//! ]                     ,
+//!                     [
+//!                     SynopsisOpt::new("--opt").description(["some simple opt"]),
+//!                     SynopsisOpt::new("--opt-with-arg").argument("ARG").description(["opt with an argument"]),
+//!                     SynopsisOpt::new("--bold")
 //!            ]),
-//!            RoffNode::example(vec![
+//!            RoffNode::paragraph(["Example:".roff().bold()]),
+//!            RoffNode::example([
 //!                r#"
 //! impl Roffable for u8 {
 //!     fn roff(&self) -> RoffText {
@@ -33,20 +41,35 @@
 //!     }
 //! }"#,
 //!             ]),
+//!            RoffNode::url("GitHub", "https://github.com/vv9k/roffman"),
+//!            RoffNode::text("\nvv9k"),
+//!            RoffNode::trademark_sign(),
 //!         ],
 //!     );
 //!
-//! let rendered = roff.to_string().unwrap();
+//!     let rendered = roff.to_string().unwrap();
+//!     println!("{}", rendered);
 //!
-//! let output = r#"
-//! .TH roffman 7 "August 2021"
+//!     let output = r#".TH roffman 7 "August 2021"
 //! .SH "BASIC USAGE"
 //! .P
 //! This is how you create a basic paragraph using roffman\.
 //! .IP "" 4
 //! This line should be slightly indented to the \fBright\.\fR
+//! .SY roffman\-command
+//! This is the description of this command\. It will be displayed right next to \fIit\fR
+//!
+//! .OP \-\-opt
+//! some simple opt
+//!
+//! .OP \-\-opt\-with\-arg ARG
+//! opt with an argument
+//!
+//! .OP \-\-bold
+//!
+//! .YS
 //! .P
-//! And some example \fIcode\fR:
+//! \fBExample:\fR
 //! .EX
 //!
 //! impl Roffable for u8 {
@@ -55,30 +78,45 @@
 //!     }
 //! }
 //! .EE
+//! .UR https://github\.com/vv9k/roffman
+//! GitHub
+//! .UE
+//!
+//! vv9k\*(Tm
 //! "#;
 //!
-//! assert_eq!(rendered.trim(), output.trim());
+//!     assert_eq!(rendered.trim(), output.trim());
+//! }
 //! ```
-//!
 //!
 //! which will look something like this:
 //! ```text
-//! roffman(7)                      Miscellaneous Information Manual                     roffman(7)
+//! roffman(7)                                         Miscellaneous Information Manual                                  roffman(7)
 //!
 //! BASIC USAGE
 //!        This is how you create a basic paragraph using roffman.
 //!
 //!            This line should be slightly indented to the right.
 //!
-//!        And some example code:
+//!        roffman-command This is the description of this command. It will be displayed right next to it
+//!
+//!                        [--opt] some simple opt
+//!
+//!                        [--opt-with-arg ARG] opt with an argument
+//!
+//!                        [--bold]
+//!
+//!        Example:
 //!
 //!        impl Roffable for u8 {
 //!            fn roff(&self) -> RoffText {
 //!                self.to_string().roff()
 //!            }
 //!        }
+//!        GitHub ⟨https://github.com/vv9k/roffman⟩
 //!
-//!                                           August 2021                                roffman(7)
+//!        vv9k™
+//!                                                               August 2021                                             roffman(7)
 //! ```
 
 use std::error::Error;
